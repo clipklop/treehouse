1da057fe4089922217b01d69591280afc3d05a8f
Misha Yartsev <siliconchik@gmail.com>
2017-02-10 22:34:54 +0300

    added two scripts: basics tests and dungeon game from outdated course

---
+++ TH_Python_Basics_Tests.py
@@ -0,0 +1,13 @@
+# #
+# #
+# #
+
+# # alpha = 'abcdefghj'
+# # print(alpha[-5])
+
+# var1 = ['a','b','c','det']
+# print(var1.index('c'))
+
+def printer(count):
+    print("Hi " * count)
+printer(2)
